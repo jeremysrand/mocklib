d871fb4660ef1b4dc2a472898e2a5014413d95fb
Jeremy Rand <jeremy@rand-family.com>
2016-09-10 16:14:16 -0400

    Initial Commit

---
+++ mocktest/main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ * mocktest
+ *
+ * Created by Jeremy Rand on 2016-09-10.
+ * Copyright (c) 2016 Jeremy Rand. All rights reserved.
+ *
+ */
+
+
+#include <stdio.h>
+#include <conio.h>
+
+
+int main(void)
+{
+    printf("HELLO, WORLD!\n");
+    cgetc();
+    return 0;
+}
